3faca067fb3cbbe3848de110de25d5a5448b47c8
Vinicius Santos Silva <santossilvavinicius0@gmail.com>
2026-03-03 22:49:07 -0300

    Criando filtro de projetos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,16 @@
+import { motion } from "motion/react"
+import "../Style/About.scss"
+
 function About() {
 
     return (
-        <>
-            <h1>Im About</h1>
-        </>
+        <div className="about-container" id="about">
+            <div className="about-header">
+                <h1>About Me</h1>
+                <div/>
+                <p>Come take a look at my projects</p>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,10 +1,14 @@
 import { useQuery } from "@tanstack/react-query"
 import { motion } from "motion/react"
+import { useState } from "react"
 import getData from "../../firebase"
 import ProjectCard from "./Especific Components/ProjectCard"
 import "../Style/Projects.scss"
+import { li } from "motion/react-client"
 
 function Projects() {
+    const [selectedTech, setSelectedTech] = useState(null)
+
     const {isPending, isError, data } = useQuery({    
         queryKey: ["ProjectsData"], 
         queryFn: () => getData("projects")
@@ -17,7 +21,31 @@ function Projects() {
     if (isError) {
         return <span>Error: {isError.message}</span>
     }
-    
+
+    const onClick = (event) => {
+        if (event.target.getAttribute("item") === selectedTech) {
+            console.log("same")
+            setSelectedTech(null)
+        } 
+        else {
+            console.log("notsame")
+            const item = event.target.getAttribute("item")
+            setSelectedTech(item)
+        }
+    }
+
+    const uniqueTechnologies = [    
+        ...new Set(
+            data.flatMap(project =>
+                project.technologies?.map(
+                    tech => tech.techName.trim()
+                ) || []
+            )   
+        )
+    ]
+
+    const filteredProjects = selectedTech ? data.filter(project => project.technologies.some(tech => tech.techName === selectedTech)) : data
+
     return (
         <div className="projects-container" id="projects">
             <div className="projects-header">
@@ -25,8 +53,25 @@ function Projects() {
                 <div/>
                 <p>Come take a look at my projects</p>
             </div>
+            <ul>
+                {
+                    uniqueTechnologies.map(techName => (
+                        <motion.li
+                            animate={{
+                                backgroundColor: selectedTech === techName ? "#007bff" : "#f8f9fa",
+                            }} 
+                            whileHover={{ backgroundColor: "#007bff" }}
+                            onClick={onClick}
+                            item={techName}
+                            key={techName}
+                        >
+                            {techName}
+                        </motion.li>
+                    ))
+                }
+            </ul>
             <motion.div className="project-grid">
-                {data.map(projectData => (
+                {filteredProjects.map(projectData => (
                     <ProjectCard key={projectData.id} dataTunnel={projectData}/>
                 ))}
             </motion.div>
